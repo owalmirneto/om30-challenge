5801fdb84fd89281d182e73217cdd8d1b2b97541
Walmir Neto <owalmirneto@gmail.com>
2023-09-19 01:30:43 -0300

    fix: add variables and skip send SMS

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  next if Rails.env.production?
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: ENV.fetch("MAILER_HOST"),
-    port: ENV.fetch("MAILER_PORT")
-  }
+    authentication: :plain,
+    address: ENV.fetch("MAILER_ADDRESS"),
+    port: ENV.fetch("MAILER_PORT"),
+    domain: ENV.fetch("MAILER_DOMAIN", nil),
+    user_name: ENV.fetch("MAILER_USER_NAME", nil),
+    password: ENV.fetch("MAILER_PASSWORD", nil)
+  }.compact
   config.action_mailer.raise_delivery_errors = false
 end
