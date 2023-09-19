7a9998d478c17613ce04b381bc7bc6caa912e136
Walmir Neto <owalmirneto@gmail.com>
2023-09-19 13:28:09 -0300

    build: enable unaccent extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_16_210742) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_150544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
