0f6e70b015fa8a9a17699cb40e6e2c9168009c70
Walmir Neto <owalmirneto@gmail.com>
2023-09-21 23:15:18 -0300

    feat: add creation of AWS S3 to github actions

--- terraform/aws_s3_buckets.tf
+++ terraform/aws_s3_buckets.tf
@@ -16,6 +16,25 @@ resource "aws_s3_bucket_acl" "devops_s3_bucket_acl" {
 resource "aws_s3_bucket_ownership_controls" "devops_s3_acl_ownership" {
   bucket = aws_s3_bucket.devops_s3_bucket.id
   rule {
-    object_ownership = "ObjectWriter"
+    object_ownership = "BucketOwnerPreferred"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "devops_s3_bucket" {
+  bucket = aws_s3_bucket.devops_s3_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "devops_s3_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.devops_s3_acl_ownership,
+    aws_s3_bucket_public_access_block.devops_s3_bucket,
+  ]
+
+  bucket = aws_s3_bucket.devops_s3_bucket.id
+  acl    = "public-read"
 }
